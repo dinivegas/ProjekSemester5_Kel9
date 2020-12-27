b3fe05bbd1935f7c8e3053456be87dce6c5dffa0
dinivegas <56098724+dinivegas@users.noreply.github.com>
2020-12-28 01:50:11 +0700

    psikologdata

--- application/controllers/admin/Data_psikolog.php
+++ application/controllers/admin/Data_psikolog.php
@@ -22,5 +22,61 @@ class Data_psikolog extends CI_Controller {
 		
 	}
 
+	public function update_aksi($id_psikolog)
+	{
+		$nama				= $this->input->post('nama');
+		$no_hp				= $this->input->post('no_hp');
+		$alamat				= $this->input->post('alamat');
+		$email				= $this->input->post('email');
+		$bidang				= $this->input->post('bidang');
+		$foto				= $this->input->post('foto');
+		
+	
+		$data	= array (
+			'nama_psikolog'					=> $nama,
+			'nohp_psikolog'					=> $no_hp,
+			'email_psikolog'				=> $email,
+			'alamat_psikolog'				=> $alamat,
+			'bidang_psikolog'				=> $bidang,
+			'foto_psikolog'					=> $foto,
+			
+		
+		);
+
+		//$this->m_psikolog->input_data($data, 'tb_psikolog');
+		$this->db->update('tb_psikolog',$data, ['id_psikolog' => $id_psikolog]);
+		redirect('admin/data_psikolog/index');
+	}
+
+	public function tambah_aksi()
+	{
+		$nama				= $this->input->post('nama');
+		$no_hp				= $this->input->post('no_hp');
+		$alamat				= $this->input->post('alamat');
+		$email				= $this->input->post('email');
+		$bidang				= $this->input->post('bidang');
+		$foto				= $this->input->post('foto');
+		
+	
+		$data	= array (
+			'nama_psikolog'					=> $nama,
+			'nohp_psikolog'					=> $no_hp,
+			'email_psikolog'				=> $email,
+			'alamat_psikolog'				=> $alamat,
+			'bidang_psikolog'				=> $bidang,
+			'foto_psikolog'					=> $foto,
+			
+		
+		);
+
+		//$this->m_psikolog->input_data($data, 'tb_psikolog');
+		$this->db->insert('tb_psikolog',$data);
+		redirect('admin/data_psikolog/index');
+	}
+
+
+	public function cekNip($nip){
+		echo  $this->db->get_where("tb_psikolog", ['nip_doswal' => $nip])->num_rows();
+	}
 
 }
